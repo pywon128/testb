eafa00bd583ee1b37454fbae392998e4d59b3032
Jaewon <dlwodnjs0128@gmail.com>
2024-07-17 16:51:41 +0900

    프로젝트 생성, 상품관리 완료

---
+++ GoodsDAO.java
@@ -0,0 +1,9 @@
+class  GoodsDAO
+{
+	public void insert(){
+		System.out.println("insert OK");
+	}
+	public void update(){
+		System.out.println("update OK");
+	}	
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello
+{
+	public static void main(String[] args) 
+	{
+		GoodsDAO dao = new GoodsDAO();
+		dao.insert();
+		dao.update();
+	}
+}
